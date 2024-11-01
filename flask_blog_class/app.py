9d3fd278a48a916d9252497dd0553e7df878552c
he-cha <afecha2001@gmail.com>
2024-11-01 05:28:51 -0500

    added edit and delete functionalitites

--- flask_blog_class/app.py
+++ flask_blog_class/app.py
@@ -18,7 +18,16 @@ def get_db_connection():
 
     #return the connection object
     return conn
+#function to retrieve a post from the database
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',(post_id,)).fetchone()
+    conn.close()
 
+    if post is None:
+        abort(404)
+
+    return post
 
 # use the app.route() decorator to create a Flask view function called index()
 @app.route('/')
@@ -55,5 +64,52 @@ def create():
             conn.close()
             return redirect(url_for('index'))
     return render_template('create.html')
+
+#create a route to edit a post. Load page with get or post method
+#pass the post id as url parameter
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    #get the post from the database with a select query for the post with that id
+    post = get_post(id)
+    #determine if the page was request with GET or POST
+    if request.method == 'POST':
+        #get the title and content
+        title = request.form['title']
+        content = request.form['content']
+
+        #if no title or content, flash error message
+        if not title:
+            flash('Title is required')
+        elif not content:
+            flash('Content is required')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ? WHERE id =?', (title, content, id))
+            conn.commit()
+            conn.close()
+
+            #redirect to the homepage
+            return redirect(url_for('index'))
+    
+    #if GET then  display page
+    return render_template('edit.html', post=post)
+#create a route to delte a post
+#delete route can only be processed with a POST method
+#the post id is a url route
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    #get the post
+    post = get_post(id)
+    #connect to the database
+    conn = get_db_connection()
+    #execute a delete query
+    conn.execute('DELETE from posts WHERE id = ?',(id,))
+    #commit and close the connection
+    conn.commit()
+    conn.close()
+    #flash a success message
+    flash('"{}" was successfully deleted'.format(post['title']))
+    #redirect to the homegpage
+    return redirect(url_for('index'))
 
 app.run(port=5008)
